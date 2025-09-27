2435dcc50369ac53c9848aceb55e5f98b983d905
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-09-27 19:33:06 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ColorPaletteKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ColorPaletteKit",
+            targets: ["ColorPaletteKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ColorPaletteKit"
+        ),
+        .testTarget(
+            name: "ColorPaletteKitTests",
+            dependencies: ["ColorPaletteKit"]
+        ),
+    ]
+)

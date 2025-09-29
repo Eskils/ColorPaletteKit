b6d6d9b38837d25b189320be1855bced472ffb5b
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-09-29 09:12:36 +0200

    Add protocol ImagePaletteComputer

---
+++ Sources/ColorPaletteKit/ImagePaletteEngine/ImagePaletteComputer.swift
@@ -0,0 +1,15 @@
+//
+//  ImagePaletteComputer.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 29/09/2025.
+//
+
+import CoreGraphics
+
+public protocol ImagePaletteComputer<Parameters> {
+    associatedtype Parameters
+    
+    init(cgImage: CGImage, parameters: Parameters)
+    func dominantColors(amount: Int) -> [SIMD3<Float>]
+}

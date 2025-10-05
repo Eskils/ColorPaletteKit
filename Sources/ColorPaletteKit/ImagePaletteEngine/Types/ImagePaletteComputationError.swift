3fab11487605183cf467dc8c8a52050d2e20178a
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-05 10:53:22 +0200

    Add model ImagePaletteComputationError

---
+++ Sources/ColorPaletteKit/ImagePaletteEngine/Types/ImagePaletteComputationError.swift
@@ -0,0 +1,11 @@
+//
+//  ImagePaletteComputationError.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 04/10/2025.
+//
+
+public enum ImagePaletteComputationError: Error {
+    case cannotMakeImageFormat
+    case cannotExtractImageData
+}

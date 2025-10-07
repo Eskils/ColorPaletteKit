9e6410603f6fa23971c876ce6bf09e929779bcd8
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-07 09:10:12 +0200

    Implement reading Int16, Int32 and ASCII in consumable data

--- Sources/ColorPaletteKit/Types/ConsumableData.swift
+++ Sources/ColorPaletteKit/Types/ConsumableData.swift
@@ -23,6 +23,54 @@ class ConsumableData {
         self.bytes = bytes
     }
     
+    func readInt16(mode: ReadMode) -> UInt16 {
+        let bytes = read(mode: mode, bytes: 2)
+        
+        let firstByte = byte(at: 0, in: bytes, or: 0)
+        let secondByte = byte(at: 1, in: bytes, or: 0)
+        
+        return UInt16(firstByte) << 8
+            + UInt16(secondByte)
+    }
+    
+    func readInt32(mode: ReadMode) -> UInt32 {
+        let bytes = read(mode: mode, bytes: 4)
+        
+        let firstByte = byte(at: 0, in: bytes, or: 0)
+        let secondByte = byte(at: 1, in: bytes, or: 0)
+        let thirdByte = byte(at: 2, in: bytes, or: 0)
+        let fourthByte = byte(at: 3, in: bytes, or: 0)
+        
+        return UInt32(firstByte) << 24
+            + UInt32(secondByte) << 16
+            + UInt32(thirdByte) << 8
+            + UInt32(fourthByte)
+    }
+    
+    func readASCII(of length: Int, mode: ReadMode) -> String {
+        let bytes = read(mode: mode, bytes: length)
+        let characters = bytes.map { Character(Unicode.Scalar($0)) }
+        return String(characters)
+    }
+    
+    func read(mode: ReadMode, bytes numberOfBytes: Int) -> ArraySlice<UInt8> {
+        switch mode {
+        case .consume:
+            consume(bytes: numberOfBytes)
+        case .read:
+            readNext(bytes: numberOfBytes)
+        }
+    }
+    
+    func readNextByte(mode: ReadMode) -> UInt8? {
+        switch mode {
+        case .consume:
+            consumeByte()
+        case .read:
+            readNextByte()
+        }
+    }
+    
     func consumeByte() -> UInt8? {
         defer {
             safelyIncrementIndex(by: 1)
@@ -62,6 +110,14 @@ class ConsumableData {
         let incrementedIndex = index + step
         let clampedIncrementedIndex = min(incrementedIndex, bytes.indices.upperBound)
         index = clampedIncrementedIndex
+    }
+    
+    private func byte(at index: Int, in bytes: ArraySlice<UInt8>, or trailing: UInt8) -> UInt8 {
+        guard bytes.indices.contains(index) else {
+            return trailing
+        }
+        
+        return bytes[index]
     }
 }
 

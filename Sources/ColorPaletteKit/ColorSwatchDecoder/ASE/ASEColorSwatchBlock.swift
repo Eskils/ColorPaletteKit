e9bef14fb7abfdd77fc2fc1e8e5b07f60533906d
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-09 09:02:15 +0200

    Add union ASEColorSwatchBlock

---
+++ Sources/ColorPaletteKit/ColorSwatchDecoder/ASE/ASEColorSwatchBlock.swift
@@ -0,0 +1,120 @@
+//
+//  ASEColorSwatchBlock.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 09/10/2025.
+//
+
+
+public enum ASEColorSwatchBlock: Equatable {
+    case group(Group)
+    case colorEntry(ColorEntry)
+}
+
+extension ASEColorSwatchBlock {
+    enum Kind {
+        case group
+        case colorEntry
+    }
+    
+    enum ExtensiveKind {
+        case groupStart
+        case groupEnd
+        case colorEntry
+    }
+    
+    public struct Group: Equatable {
+        public let name: String
+        public let components: [ASEColorSwatchBlock]
+        
+        public init(name: String, components: [ASEColorSwatchBlock]) {
+            self.name = name
+            self.components = components
+        }
+    }
+    
+    public struct ColorEntry: Equatable {
+        public let name: String
+        public let colorModel: ColorModel
+        public let colorType: ColorType
+        public let components: [Float]
+        
+        public init(name: String, colorModel: ColorModel, colorType: ColorType, components: [Float]) {
+            self.name = name
+            self.colorModel = colorModel
+            self.colorType = colorType
+            self.components = components
+        }
+    }
+    
+    public enum ColorModel: Equatable {
+        case rgb
+        case cmyk
+        case lab
+        case gray
+    }
+    
+    public enum ColorType: Equatable {
+        case global
+        case spot
+        case normal
+    }
+}
+
+extension ASEColorSwatchBlock {
+    var kind: Kind {
+        switch self {
+        case .group:
+                .group
+        case .colorEntry:
+                .colorEntry
+        }
+    }
+}
+
+extension ASEColorSwatchBlock.ExtensiveKind {
+    init?(identifier: UInt16) {
+        switch identifier {
+        case 0xc001: self = .groupStart
+        case 0xc002: self = .groupEnd
+        case 0x01: self = .colorEntry
+        default: return nil
+        }
+    }
+}
+
+extension ASEColorSwatchBlock.ColorModel {
+    init?(identifier: String) {
+        switch identifier {
+        case "RGB ": self = .rgb
+        case "CMYK": self = .cmyk
+        case "LAB ": self = .lab
+        case "GRAY": self = .gray
+        default: return nil
+        }
+    }
+    
+    var numberOfComponents: Int {
+        switch self {
+        case .rgb:
+            3
+        case .cmyk:
+            4
+        case .lab:
+            3
+        case .gray:
+            1
+        }
+    }
+}
+
+extension ASEColorSwatchBlock.ColorType {
+    init?(identifier: UInt16) {
+        switch identifier {
+        case 0x0: self = .global
+        case 0x1: self = .spot
+        case 0x2: self = .normal
+        default: return nil
+        }
+    }
+}

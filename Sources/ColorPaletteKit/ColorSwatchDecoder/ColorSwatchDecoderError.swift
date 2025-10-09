3246faea56eaf02a9ab5c8609ab5f07bc3f6f20d
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-09 09:01:42 +0200

    Add type ColorSwatchDecoderError

---
+++ Sources/ColorPaletteKit/ColorSwatchDecoder/ColorSwatchDecoderError.swift
@@ -0,0 +1,15 @@
+//
+//  ColorSwatchDecoderError.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 05/10/2025.
+//
+
+
+public enum ColorSwatchDecoderError: Error {
+    case invalidFormat(received: String, expected: String)
+    case unsupportedVersion(major: Int, minor: Int)
+    case invalidBlockType(typeIdentifier: UInt16)
+    case invalidColorModel(identifier: String)
+    case invalidColorType(identifier: UInt16)
+}

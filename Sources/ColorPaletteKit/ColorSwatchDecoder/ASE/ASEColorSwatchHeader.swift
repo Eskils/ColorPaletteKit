174176b2c3a5f989b5808fa370edcf974f3b054c
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-09 09:01:55 +0200

    Add internal type ASEColorSwatchHeader

---
+++ Sources/ColorPaletteKit/ColorSwatchDecoder/ASE/ASEColorSwatchHeader.swift
@@ -0,0 +1,13 @@
+//
+//  ASEColorSwatchHeader.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 09/10/2025.
+//
+
+
+struct ASEColorSwatchHeader {
+    let format: String
+    let version: (major: UInt16, minor: UInt16)
+    let numberOfBlocks: UInt32
+}

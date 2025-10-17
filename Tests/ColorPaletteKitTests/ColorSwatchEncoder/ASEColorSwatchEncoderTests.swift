865afc5eb89e751e2b79ad8e432bb9a58008ee83
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-17 22:33:46 +0200

    Add test for encoding single color

--- Tests/ColorPaletteKitTests/ColorSwatchEncoder/ASEColorSwatchEncoderTests.swift
+++ Tests/ColorPaletteKitTests/ColorSwatchEncoder/ASEColorSwatchEncoderTests.swift
@@ -22,4 +22,36 @@ struct ASEColorSwatchEncoderTests {
         let bytes = [UInt8](data)
         #expect(bytes == expectedBytes)
     }
+    
+    @Test
+    func encodesSingleColor() throws {
+        let expectedBytes: [UInt8] = [
+            65, 83, 69, 70,
+            0, 1, 0, 0,
+            0, 0, 0, 1,
+            0, 1,
+            0, 0, 0, 22,
+            0, 1, 0, 0,
+            82, 71, 66, 32,
+            63, 0, 0, 0,
+            63, 0, 0, 0,
+            63, 0, 0, 0,
+            0, 0
+        ]
+        let encoder = ASEColorSwatchEncoder()
+        let data = try encoder.encode(
+            [
+                .colorEntry(
+                    ASEColorSwatchBlock.ColorEntry(
+                        name: "",
+                        colorModel: .rgb,
+                        colorType: .global,
+                        components: [0.5, 0.5, 0.5]
+                    )
+                )
+            ]
+        )
+        let bytes = [UInt8](data)
+        #expect(bytes == expectedBytes)
+    }
 }

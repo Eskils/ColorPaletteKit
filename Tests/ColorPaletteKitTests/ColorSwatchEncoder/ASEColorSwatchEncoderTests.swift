a877bc1e70a65143672ebe1b4f7afa78361ef8e7
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-18 22:21:34 +0200

    Test encoding ASE color swatch with group

--- Tests/ColorPaletteKitTests/ColorSwatchEncoder/ASEColorSwatchEncoderTests.swift
+++ Tests/ColorPaletteKitTests/ColorSwatchEncoder/ASEColorSwatchEncoderTests.swift
@@ -10,6 +10,12 @@ import Testing
 import ColorPaletteKit
 
 struct ASEColorSwatchEncoderTests {
+    private let testsDirectory = URL(fileURLWithPath: #filePath + "/../../../").standardizedFileURL
+        
+    func filePath(name: String, directory: String) -> String {
+        "\(testsDirectory.path)/\(directory)/\(name)"
+    }
+    
     @Test
     func encodesHeaderOfEmptySwatch() throws {
         let expectedBytes: [UInt8] = [
@@ -53,5 +59,65 @@ struct ASEColorSwatchEncoderTests {
         )
         let bytes = [UInt8](data)
         #expect(bytes == expectedBytes)
+    }
+    
+    @Test
+    func encodesGroup() throws {
+        let path = filePath(name: "group-palette.ase", directory: "TestAssets/Swatches/ASE")
+        let expectedData = try Data(contentsOf: URL(filePath: path))
+        let encoder = ASEColorSwatchEncoder()
+        let data = try encoder.encode(
+            [
+                .group(
+                    ASEColorSwatchBlock.Group(
+                        name: "Adobe Swatch Exchange file",
+                        components: [
+                            .colorEntry(
+                                ASEColorSwatchBlock.ColorEntry(
+                                    name: "",
+                                    colorModel: .rgb,
+                                    colorType: .normal,
+                                    components: [0.0196078, 0.580392, 0.509804]
+                                )
+                            ),
+                            .colorEntry(
+                                ASEColorSwatchBlock.ColorEntry(
+                                    name: "",
+                                    colorModel: .rgb,
+                                    colorType: .normal,
+                                    components: [0.0, 0.619608, 0.25098]
+                                )
+                            ),
+                            .colorEntry(
+                                ASEColorSwatchBlock.ColorEntry(
+                                    name: "",
+                                    colorModel: .rgb,
+                                    colorType: .normal,
+                                    components: [0.0470588, 0.529412, 0.0]
+                                )
+                            ),
+                            .colorEntry(
+                                ASEColorSwatchBlock.ColorEntry(
+                                    name: "",
+                                    colorModel: .rgb,
+                                    colorType: .normal,
+                                    components: [0.427451, 0.619608, 0.0313726]
+                                )
+                            ),
+                            .colorEntry(
+                                ASEColorSwatchBlock.ColorEntry(
+                                    name: "",
+                                    colorModel: .rgb,
+                                    colorType: .normal,
+                                    components: [0.580392, 0.537255, 0.0]
+                                )
+                            )
+                        ]
+                    )
+                )
+            ]
+        )
+        let bytes = [UInt8](data)
+        #expect(bytes == [UInt8](expectedData))
     }
 }

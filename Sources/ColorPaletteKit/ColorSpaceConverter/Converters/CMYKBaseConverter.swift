61765a25d19c6f304939a596f0f52eba2c7386c6
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-19 15:16:58 +0200

    Add dummy base converters

---
+++ Sources/ColorPaletteKit/ColorSpaceConverter/Converters/CMYKBaseConverter.swift
@@ -0,0 +1,14 @@
+//
+//  CMYKBaseConverter.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 19/10/2025.
+//
+
+struct CMYKBaseConverter: ColorConverterComputer {
+    let target: ColorSpaceKind
+    
+    func convert(colorValues: [Float]) throws(ColorConverterError) -> [Float] {
+        []
+    }
+}
---
+++ Sources/ColorPaletteKit/ColorSpaceConverter/Converters/LABBaseConverter.swift
@@ -0,0 +1,14 @@
+//
+//  LABBaseConverter.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 19/10/2025.
+//
+
+struct LABBaseConverter: ColorConverterComputer {
+    let target: ColorSpaceKind
+    
+    func convert(colorValues: [Float]) throws(ColorConverterError) -> [Float] {
+        []
+    }
+}

bb7b14572f0d7774aa297ce37975a6705f591013
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-19 15:13:12 +0200

    Add enum ColorSpaceKind

---
+++ Sources/ColorPaletteKit/ColorSpaceConverter/Types/ColorSpaceKind.swift
@@ -0,0 +1,14 @@
+//
+//  ColorSpaceKind.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 19/10/2025.
+//
+
+public enum ColorSpaceKind {
+    case rgb
+    case cmyk
+    case lab
+    case hsb
+    case gray
+}

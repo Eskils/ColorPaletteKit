e61017544f63d306bc5506421f204643f1551391
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-25 09:23:18 +0200

    Add documentation comments to PaletteImageRenderer

--- Sources/ColorPaletteKit/PaletteImageEngine/PaletteImageRenderer.swift
+++ Sources/ColorPaletteKit/PaletteImageEngine/PaletteImageRenderer.swift
@@ -7,10 +7,20 @@
 
 import CoreGraphics
 
+/// Implement this protocol to define a new PaletteImageRenderer.
+/// You may then add a new case to ``PaletteImageRendererKind``
+/// to allow using this renderer in the ``PaletteImageEngine``.
 public protocol PaletteImageRenderer {
+    /// Render `colors` to an image of the requested `size`.
+    /// - Parameters:
+    ///   - colors: A list of colors in the RGB color space
+    ///   - size: The size of the image
+    /// - Returns: An image with the rendered colors.
     func render(colors: [SIMD3<Float>], size: CGSize) throws(PaletteImageRendererError) -> CGImage
 }
 
+/// Possible errors that could occur when rendering a palette image
 public enum PaletteImageRendererError: Error {
+    /// The image could not be rendered due to the underlying error.
     case renderFailure(underlying: Error)
 }

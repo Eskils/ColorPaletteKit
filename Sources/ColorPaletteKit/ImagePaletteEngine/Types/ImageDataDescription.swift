a3f86a7c12fb67e325e8884ffc14e5acb3ca7f0e
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-29 08:22:19 +0100

    Add type ImageDataDescription

---
+++ Sources/ColorPaletteKit/ImagePaletteEngine/Types/ImageDataDescription.swift
@@ -0,0 +1,33 @@
+//
+//  ImageDataDescription.swift
+//  ColorPaletteKit
+//
+//  Created by Eskil Gjerde Sviggum on 28/10/2025.
+//
+
+import Accelerate
+
+public struct ImageDataDescription {
+    public let data: UnsafeMutablePointer<Float>
+    public let width: Int
+    public let height: Int
+    public let components: Int
+    
+    init(data: UnsafeMutablePointer<Float>, width: Int, height: Int, components: Int) {
+        self.data = data
+        self.width = width
+        self.height = height
+        self.components = components
+    }
+}
+
+extension ImageDataDescription {
+    func imageBuffer() -> vImage_Buffer {
+        vImage_Buffer(
+            data: UnsafeMutableRawPointer(data),
+            height: UInt(height),
+            width: UInt(width),
+            rowBytes: width * components
+        )
+    }
+}
